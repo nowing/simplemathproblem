c57f736762ed8680900b960394f9881ea61fc792
nowing <nowing@sina.com>
2017-04-24 15:40:45 +0800

    除法题目1/3带余数 9天一次，这样答案正好一页

--- create.py
+++ create.py
@@ -8,7 +8,7 @@ import random
 import datetime
 
 
-def getStep(calcType, len1=1, len2=1):
+def getStep(calcType, len1=1, len2=1, divideExactly = False):
     a = random.randint(10**(len1-1), 10**len1)
     b = random.randint(10**(len2-1), 10**len2)
     if calcType == "add":
@@ -18,7 +18,14 @@ def getStep(calcType, len1=1, len2=1):
     elif calcType == "multiple":
         t = (str(a) + " × " + str(b), a*b )
     elif calcType == "divide":
-        t = (str(a*b) + " ÷ " + str(b), a)
+        if divideExactly == False:
+            c = random.randint(0, b)
+        else:
+            c = 0
+        if divideExactly == False:
+            t = (str(a*b+c) + " ÷ " + str(b), str(a) + "..." + str(c))
+        else:
+            t = (str(a*b) + " ÷ " + str(b), str(a))
     return t
 
 def createOneDay():
@@ -45,7 +52,10 @@ def createOneDay():
     
     answer.append("除:")
     for i in range(10):
-        one = getStep("divide", 2, 2)
+        divideE = False
+        if random.randint(1,10) <= 7:   #三分之一不能整除 
+            divideE = True
+        one = getStep("divide", 2, 2, divideE)
         lines.append(one[0] + " = ")
         answer.append(one[1])
         
@@ -55,7 +65,7 @@ def createOneDay():
 filename= datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".txt" 
 with open("/"+filename, "w") as f:
     dayanswer = []
-    for day in range(5):
+    for day in range(9):
         f.write("Day " + str(day + 1) + " : " +"\n\n")
         oneday = createOneDay()
         i = 0
@@ -68,10 +78,14 @@ with open("/"+filename, "w") as f:
                 f.write("\t\t\t\t\t\t\t\t")
         f.write("\n\n")
         
-        daya = ""
+        daya = "Day " + str(day + 1) + " : "
         for word in oneday[1]:
             daya = daya + str(word) + "  "
         dayanswer.append(daya)
 
     for word in dayanswer:
-        f.write(str(word) + "  " + "\n\n\n")
+        f.write(str(word) + "  " + "\n\n")
+
+with open("/"+filename, "r") as f:
+    line = f.readlines()
+    print(line)
