3db4b9f9974e36b3f730ae1e14abdbec7b18a2f5
nowing <nowing@sina.com>
2018-04-27 21:18:54 +0800

    添加小数除法

--- create.py
+++ create.py
@@ -5,94 +5,77 @@ This is a temporary script file.
 """
 import random
 import datetime
-from decimal import Decimal
-#from decimal import getcontext
+import math
 
-
-class AddCreator(object):
-    pass
-
-class MinusCreator(object):
-    pass
-
-class MultipleCreator(object):
-    pass
-
-class DivideCreator(object):
-    pass
-
-
-
-
-def getStep(calcType, len1=1, len2=1, divideExactly = False, withPoint = 0):
-    if withPoint == 0:
-        a = random.randint(10**(len1-1), 10**len1)
-        b = random.randint(10**(len2-1), 10**len2)
-    else:
-        a = round(random.randint(10**(len1-1+withPoint), 10**(len1+withPoint)) / (10**withPoint), withPoint)
-        b = round(random.randint(10**(len2-1+withPoint), 10**(len2+withPoint)) / (10**withPoint), withPoint)
-        
+def getStep(calcType, len1=1, len2=1, divideType = 'ZC'):
+    # divideType: ZC=整除   YS=余数   XS=小数
+    a = random.randint(10**(len1-1), 10**len1)
+    b = random.randint(10**(len2-1), 10**len2)
+    c = 0
     if calcType == "add":
-        t = (str(a) + " + " + str(b), Decimal(str(a)) + Decimal(str(b)))
+        t = (str(a) + " + " + str(b), a+b )
     elif calcType == "minus":
-        t = (str((Decimal(str(a)) + Decimal(str(b)))) + " - " + str(b), str(a))
+        t = (str(a+b) + " - " + str(b), a)
     elif calcType == "multiple":
-        t = (str(a) + " × " + str(b), Decimal(str(a)) * Decimal(str(b)) )
+        scale = math.pow(10, random.randint(0,1))
+        scale2 = math.pow(10, random.randint(0,1))
+
+        t = (str(a) + " × " + str(b), a*b )
     elif calcType == "divide":
-        if withPoint == 0:
-            if divideExactly == False:
-                c = random.randint(0, b)
-            else:
-                c = 0
-            if divideExactly == False:
-                t = (str(a*b+c) + " ÷ " + str(b), str(a) + "..." + str(c))
-            else:
-                t = (str(a*b) + " ÷ " + str(b), str(a))
-        else:
-            t = (str((Decimal(str(a)) * Decimal(str(b)))) + " ÷ " + str(b), str(a))
+        if divideType == 'YS':
+            c = random.randint(0, b)
+                 
+        if divideType == 'YS':
+            t = (str(a*b+c) + " ÷ " + str(b), str(a) + "..." + str(c))
+        elif divideType == 'ZC':
+            t = (str(a*b) + " ÷ " + str(b), str(a))
+        else : # XS
+            scale = math.pow(10, random.randint(1,2))
+            scale2 = math.pow(10, random.randint(0,1))
+            scale_t = scale * scale2
+            t = (str(a*b/scale_t)) + " ÷ " + str(b/scale2), str(a/scale)
     return t
 
-def createOneDay(perday=10):
+def createOneDay():
     lines=[]
     answer=[]
     
     answer.append("加:")
-    for i in range(perday):
-        one = getStep("add", 3, 3, False, 1)
+    for i in range(5):
+        one = getStep("add", 3, 3)
         lines.append(one[0] + " = ")
         answer.append(one[1])
     
     answer.append("减:")
-    for i in range(perday):
-        one = getStep("minus", 3, 3, False, 1)
+    for i in range(5):
+        one = getStep("minus", 3, 3)
         lines.append(one[0] + " = ");
         answer.append(one[1])
     
     answer.append("乘:")
-    for i in range(perday):
-        one = getStep("multiple", 3, 2, False, 1)
+    for i in range(5):
+        one = getStep("multiple", 3, 2)
         lines.append(one[0] + " = ")
         answer.append(one[1])
     
     answer.append("除:")
-    for i in range(perday):
-#        divideE = False
-#        if random.randint(1,10) <= 5:   #一半不能整除 
-#            divideE = True
-        one = getStep("divide", 2, 2, False, 1)
+    for i in range(5):
+        divideE = 'ZC'
+        #if random.randint(1,10) <= 5:   #一半不能整除 
+        divideE = 'XS'
+        one = getStep("divide", 2, 2, divideE)
         lines.append(one[0] + " = ")
         answer.append(one[1])
         
     return (lines, answer)
     
     
-filename= datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".txt" 
-with open("d:/"+filename, "w") as f:
+filename= "c:\\temp\\" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".txt" 
+with open(filename, "w") as f:
     dayanswer = []
-    days = 10
-    for day in range(days):
+    for day in range(10):
         f.write("Day " + str(day + 1) + " : " +"\n\n")
-        oneday = createOneDay(5)
+        oneday = createOneDay()
         i = 0
         for line in oneday[0]:
             f.write(line)
@@ -101,7 +84,7 @@ with open("d:/"+filename, "w") as f:
                 f.write("\n\n")
             else:
                 f.write("\t\t\t\t\t\t\t\t")
-        f.write("\n")
+        #f.write("\n\n")
         
         daya = "Day " + str(day + 1) + " : "
         for word in oneday[1]:
@@ -111,6 +94,6 @@ with open("d:/"+filename, "w") as f:
     for word in dayanswer:
         f.write(str(word) + "  " + "\n\n")
 
-with open("d:/"+filename, "r") as f:
+with open(filename, "r") as f:
     line = f.readlines()
     print(line)
